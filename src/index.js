0078be5cc93a906cb52f512ec63ddca7ba336029
mardelys <mardelys1996@gmail.com>
2022-11-24 09:21:20 -0500

    Avances Prácticas e ideas para la animación

--- src/index.js
+++ src/index.js
@@ -2,6 +2,40 @@
 
 //Mardelys
 
+const btnplay = document.getElementById('#play-ruleta');
+const animacioRuleta = document.getElementById('#animacion-ruleta');
+class ANIMACION{
+   constructor(animacioRuleta){
+   this.canPlay = false;
+}
+}
+
+
+function rotacionRuleta(){
+   gsap.to('.ruleta', {
+   duration: 1,
+   rotate : '360deg',
+   x: 0,
+
+})
+
+}
+
+function rotacionHacha(){
+    gsap.to('.hacha',{
+   duration : 1,
+   rotate : '700deg',
+   x: -100,
+   y: -350,
+   scale: 0.5,
+})
+}
+   
+   
+
+btnplay.addEventListener("click", (e) => {
+   rotacionRuleta()
+})construc
 //Bryan
 
 //Angela
