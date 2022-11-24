cef824057c5677e54137bd15371186d216b291fd
mardelys <mardelys1996@gmail.com>
2022-11-24 18:03:48 -0500

    Avances animación

--- src/index.js
+++ src/index.js
@@ -2,45 +2,63 @@
 
 //Mardelys
 
-var $playRuleta = document.getElementById('#play-ruleta');
 var tl = gsap.timeline();
+const $playRuleta = document.querySelector('#play-ruleta');
 
+$playRuleta.addEventListener('click', (e) => {
+   e.preventDefault();
+   document.value = true;
+   for(i = 0;document.value === true;i++) {
+      return playRuleta();
+   };
+});
 
 function playRuleta(){
-      for(i=0; $playRuleta === true; i++){
-      tl.to('.ruleta', {
-         duration: 1,
-         rotate : '360deg',
-         x: 0,
-         repeat: 1,
-      },'0')
+   tl.to('.ruleta', {
+      duration: 1.5,
+      rotate : '360deg',
+      x: 0,
+      repeat: 1,
+   },'0')
+
+
+   tl.to('.cuerpo-ruleta', {
+      duration: 1.5,
+      rotate : '360deg',
+      x: 0,
+      y: 0,
+      repeat: 1,
+      display: 'none',
+   },'-=3')
    
-      tl.to('.cuerpo-ruleta', {
-         duration: 1,
-         rotate : '360deg',
-         x: 0,
-         repeat: 1,
-      },'-=2')
    
-      tl.to('.hacha',{
-         duration : 1,
-         rotate : '700deg',
-         x: 5,
-         y: -450,
-         scale: 0.5,
-      },'-=1')
-      tl.from('.hacha',{
-         duration: 0.5,
-         ease: 'linear',
-         x:0,
-         y:-10,
-         scale:2,
-      },'-=2')
-   }
+
+   tl.to('.hacha',{
+      duration : 1,
+      rotate : '700deg',
+      x: 0,
+      y: -450,
+      scale: 0.5,
+   },'-=0.8')
+   
+   tl.to('.cuerpo-ruleta2', {
+      duration: 1.5,
+      rotate : '-10deg',
+      x: 0,
+      y: 0,
+      repeat: 0,
+      display: 'block',
+   },'-=0.1')
+   tl.to('.cuerpo-ruleta1', {
+      duration: 1.5,
+      rotate : '10deg',
+      x: 0,
+      y: 0,
+      repeat: 0,
+      display: 'block',
+   },'-=1.6')
 
 };
-
-
 
 //Bryan
 
