f10382290ce27b3cd3c50768a63813b8bc218f92
ElPardo16 <andresito105@gmail.com>
2022-11-28 14:05:34 -0500

    mejoras en javascript y funcion de sacrificar

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ var saveListNames = [
 ]
 var listNamesGame = [...saveListNames]
 var deads = 0;
+var animationTime = 2000;
 //Robinson
 
 //Mardelys
@@ -24,6 +25,14 @@ var deads = 0;
 //Angela
 
 //Andres
+function delay(seconds){
+    seconds = seconds || 2000;
+    return new Promise(done => {
+      setTimeout(() => {
+        done();
+      }, seconds);
+    });
+}
 function drawArray(names){
     lista.innerHTML = ""
     names.forEach(({name, sacrificado}) => {
@@ -36,11 +45,15 @@ function drawArray(names){
 }
 function addName(){
     if (!listNamesGame.some(({name}) => name == inputName.value.toLowerCase())){
-        listNamesGame.push({
-            name: inputName.value.toLowerCase(),
-            sacrificado: false
-        })
-        drawArray(listNamesGame)
+        if(inputName.value != ""){
+            listNamesGame.push({
+                name: inputName.value.toLowerCase(),
+                sacrificado: false
+            })
+            drawArray(listNamesGame)
+        }else{
+            console.log("Vacio")
+        }
     }else{
         console.log("repite")
     }
@@ -50,7 +63,7 @@ function deleteName(element){
     listNamesGame = listNamesGame.filter(({name}) => name != txtName)
     drawArray(listNamesGame)
 }
-function sacrifice(victims){
+async function sacrifice(victims){
     let ready = false
     let indexRandom = Math.floor(Math.random() * listNamesGame.length)
     while(!ready){
@@ -58,16 +71,38 @@ function sacrifice(victims){
             if(victims[indexRandom].sacrificado){
                 indexRandom = Math.floor(Math.random() * listNamesGame.length)
             }else{
-                ready = true
+                /* ready = true */
+                console.log("1 menos")
+                await delay(1000)
+                // llamar animacion
                 victims[indexRandom].sacrificado = true
+                drawArray(victims)
                 deads++
             }
         }else{
             ready = true
-            console.log("se acabaron")
+            //dialogo
+            console.log("se acabaron Game over")
+            await delay(1000)
+            alert("perdio")
+            document.location.href = "game-over.html"
         }
+        //drawArray(listNamesGame)
     }
-    drawArray(listNamesGame)
+}
+function animationRandom(){
+    // hacer variable indexAnimation y asignar numero random entre 0 y 3
+    switch(indexAnimation){
+        case 0:
+            //hace algo
+            // cambiar valor de animationTime al tiempo que dura la anim
+            animationTime = 3000
+            break;
+        case 1:
+            //hace algo
+            break;
+        // terminar
+    }
 }
 function restartGame(){
 
@@ -75,8 +110,12 @@ function restartGame(){
 function soundFX(){
 
 }
+function playGame(){
+    sacrifice(listNamesGame)
+    btnPlay.classList.add("disable")
+}
 drawArray(listNamesGame)
-setTimeout(() => sacrifice(listNamesGame), 2000)
+//setTimeout(() => sacrifice(listNamesGame), 2000)
 /* setTimeout(() => sacrifice(listNamesGame), 4000)
 setTimeout(() => sacrifice(listNamesGame), 6000)
 setTimeout(() => sacrifice(listNamesGame), 12000) */
