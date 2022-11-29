a869ad81ac5d7ad98420b21b5078ae2653ffac7f
Angela Diaz <angeldi846@gmail.com>
2022-11-28 21:43:51 -0500

    Animación del verdugo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 
 //Angela
 
+
 let tL = gsap.timeline ({
     repeat: Infinity,
     yoyo: true,
@@ -15,7 +16,7 @@ tL.to('.spectators-shadow',{
     duration: 2,
     y: 25,
     borderBottom: 10 ,
-})
+});
 
 gsap.to('.blade',{
     duration:.8,
@@ -26,6 +27,7 @@ gsap.to('.blade',{
 gsap.to('.head-sacrifice',{
     duration:.7,
     y : 105,
+    x : 20,
     delay: .5,
     ease: Expo.easeIn
 })
@@ -37,6 +39,43 @@ gsap.fromTo('.blade-blood', {
     delay:.7,
     duration:.3,
 })
+
+gsap.fromTo('.sprite', {
+    opacity:0,
+},{
+    opacity:.8,
+    delay:2,
+    duration:.3,
+})
+
+//sprite
+function sprite () {
+    let step = 1
+    const movement = 369
+    const maxStep = 6
+
+    setTimeout(()=>{
+        setInterval(() => {
+            const sprite = document.querySelector('.sprite')
+            if(step >= maxStep){
+                return
+            }
+            const x = step * movement
+            step = step > maxStep ? 1 : step + 1
+            sprite.style.backgroundPosition = `-${x}px 0px`
+        }, 70)
+        gsap.to('.head-sacrifice',{
+            duration:1,
+            x : 1000,
+            y : -300,
+            delay: .5,
+            ease: Expo.Out
+        })
+    },2000)
+
+}
+
+sprite ()
 
 
 //Andres
