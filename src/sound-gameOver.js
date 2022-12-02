ea4aaea4c53e130de04ad41af49ade3724da238a
mardelys <mardelys1996@gmail.com>
2022-12-02 12:11:01 -0500

    Commit para merge

---
+++ src/sound-gameOver.js
@@ -0,0 +1,5 @@
+let sounds = new Audio('./src/sounds/game-over-sonido.mp3');
+
+window.onload = function startSound(){
+  sounds.play();
+};
--- src/sounds.js
+++ src/sounds.js
@@ -1,9 +1,5 @@
 let sounds = new Audio('./src/sounds/fondo 1 .mp3');
-let $soundNote = document.querySelector('')
+
 window.onload = function startSound(){
   sounds.play();
 };
-$playRuleta.addEventListener('click', (e) => {
-   e.preventDefault();
-  sounds.pause();
-});
